c804e389f1410e7fb08a56e4700c62af19f2f195
ronakpustack <ronak@pustack.com>
2024-03-18 21:19:27 +0530

    Re-included functions

--- quickbytes_cloud/functions/src/index.ts
+++ quickbytes_cloud/functions/src/index.ts
@@ -1,11 +1,9 @@
 import { onNewsPublish, publishNews, removeNews } from './routes/news.routes'
 import { notifyTopic } from './routes/notifications.route'
 
-// cd quickbytes_cloud/functions
-
 export {
     notifyTopic,
-    // onNewsPublish,
-    // publishNews,
-    // removeNews,
+    onNewsPublish,
+    publishNews,
+    removeNews,
 }
