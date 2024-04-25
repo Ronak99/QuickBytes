6a963b3a235c6af9ddbcc233ea8dc80faa684d52
Ronak00 <punase.ronak99@gmail.com>
2024-04-25 22:20:51 +0530

    News card update

--- quickbytes_app/lib/features/news/presentation/pages/news_page.dart
+++ quickbytes_app/lib/features/news/presentation/pages/news_page.dart
@@ -1,12 +1,20 @@
+import 'dart:ffi';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
+import 'package:news_repository/news_repository.dart';
+
 import 'package:quickbytes_app/core/logs/logs.dart';
-import 'package:quickbytes_app/shared/widgets/blur_view.dart';
-import 'package:quickbytes_app/shared/widgets/cached_image.dart';
 import 'package:quickbytes_app/features/news/presentation/widgets/article_card/article_card.dart';
 import 'package:quickbytes_app/features/news/state/news_bloc.dart';
+import 'package:quickbytes_app/shared/widgets/blur_view.dart';
+import 'package:quickbytes_app/shared/widgets/cached_image.dart';
+
+final pageStorageBucket = PageStorageBucket();
+
+final cardSwiperController = CardSwiperController();
 
 class NewsPage extends StatefulWidget {
   const NewsPage({super.key});
@@ -15,7 +23,8 @@ class NewsPage extends StatefulWidget {
   State<NewsPage> createState() => _NewsPageState();
 }
 
-class _NewsPageState extends State<NewsPage> {
+class _NewsPageState extends State<NewsPage>
+    with AutomaticKeepAliveClientMixin {
   @override
   void initState() {
     super.initState();
@@ -24,6 +33,8 @@ class _NewsPageState extends State<NewsPage> {
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
+
     return Scaffold(
       body: BlocBuilder<NewsBloc, NewsState>(
         builder: (context, state) {
@@ -32,9 +43,6 @@ class _NewsPageState extends State<NewsPage> {
               child: CupertinoActivityIndicator(),
             );
           } else if (state is NewsLoaded) {
-            Logger.instance.i(state.index, stackTrace: StackTrace.empty);
-            final newsBloc = context.read<NewsBloc>();
-
             return Stack(
               children: [
                 // Separated out the background widget responsible for a blur background
@@ -61,26 +69,8 @@ class _NewsPageState extends State<NewsPage> {
                 if (state.articles.isEmpty)
                   const Center(child: Text('You are all caught up!'))
                 else
-                  CardSwiper(
-                    cardsCount: state.articles.length,
-                    padding: EdgeInsets.zero,
-                    controller: newsBloc.cardSwiperController,
-                    isLoop: true,
-                    onSwipe: (previousIndex, currentIndex, direction) {
-                      if (currentIndex != null) {
-                        // using a proper logging package instead of primitive print provided by dart
-                        Logger.instance
-                            .w(currentIndex, stackTrace: StackTrace.empty);
-                        newsBloc.add(ArticleSelectedAtIndex(currentIndex));
-                      }
-
-                      return true;
-                    },
-                    cardBuilder: (context, index, percentThresholdX,
-                            percentThresholdY) =>
-                        state.articles
-                            .map((e) => ArticleCard(article: e))
-                            .toList()[index],
+                  NewsPageView(
+                    articles: state.articles,
                   ),
               ],
             );
@@ -90,6 +80,63 @@ class _NewsPageState extends State<NewsPage> {
           );
         },
       ),
+    );
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
+
+class NewsPageView extends StatelessWidget {
+  final List<Article> articles;
+
+  const NewsPageView({
+    Key? key,
+    required this.articles,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      itemCount: articles.length,
+      controller: context.read<NewsBloc>().cardSwiperController,
+      padEnds: false,
+      scrollDirection: Axis.vertical,
+      onPageChanged: (index) {
+        final newsBloc = context.read<NewsBloc>();
+        newsBloc.add(ArticleSelectedAtIndex(index));
+      },
+      itemBuilder: (context, index) =>
+          articles.map((e) => ArticleCard(article: e)).toList()[index],
+    );
+  }
+}
+
+class NewsSwiper extends StatelessWidget {
+  final List<Article> articles;
+
+  const NewsSwiper({
+    Key? key,
+    required this.articles,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CardSwiper(
+      controller: cardSwiperController,
+      cardsCount: articles.length,
+      padding: EdgeInsets.zero,
+      isLoop: true,
+      onSwipe: (previousIndex, currentIndex, direction) {
+        if (currentIndex != null) {
+          final newsBloc = context.read<NewsBloc>();
+          newsBloc.add(ArticleSelectedAtIndex(currentIndex));
+        }
+
+        return true;
+      },
+      cardBuilder: (context, index, percentThresholdX, percentThresholdY) =>
+          articles.map((e) => ArticleCard(article: e)).toList()[index],
     );
   }
 }
